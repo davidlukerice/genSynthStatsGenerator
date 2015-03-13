505d757262a0fe9acdba9391e9540107be4c217f
davidlukerice <davidlukerice@gmail.com>
2015-03-12 19:25:06 -0500

    add some stub data

---
+++ collections/users.js
@@ -0,0 +1,3 @@
+var users = [
+    { "id" : "544546eb2d6125771fc46afe", "username" : "ur347", "provider" : "local", "created" : "2014-10-20T17:31:23.068Z", "stars" : [ ], "unpublishedCount" : 6, "instruments" : [ "544546ee2d6125771fc46aff", "544546ef2d6125771fc46b00", "544546f02d6125771fc46b01", "544546f12d6125771fc46b02", "544546f12d6125771fc46b03", "544546f32d6125771fc46b04" ] },
+];
