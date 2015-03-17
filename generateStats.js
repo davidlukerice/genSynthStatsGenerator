50782724bc04e7a6964b54d185b97fdf392235b0
davidlukerice <davidlukerice@gmail.com>
2015-03-17 00:10:51 -0500

    track evolution history

--- generateStats.js
+++ generateStats.js
@@ -94,6 +94,18 @@ $(function() {
         return _.findIndex(me, {id:instrument.user}) === -1;
     }).length);
 
+    var publishedInstruments = _.filter(instruments, function(instrument) {
+        return !instrument.isPrivate;
+    });
+    var publishedInstrumentJSONs = _.map(publishedInstruments, function(instrument) {
+        return JSON.parse(instrument.json);
+    });
+    log('published: ' + publishedInstruments.length);
+
+    log('published notMe: ' + _.filter(instruments, function(instrument) {
+        return !instrument.isPrivate && _.findIndex(me, {id:instrument.user}) === -1;
+    }).length);
+
     var instrumentJSONs = _.map(instruments, function(instrument) {
         return JSON.parse(instrument.json);
     });
@@ -130,7 +142,51 @@ $(function() {
     var generationDepthAvg = _.sum(_.map(instrumentJSONs, 'generation'), 'generation') / instrumentJSONs.length;
     log('generation depth: max: '+generationDepthMax+', avg:'+generationDepthAvg);
 
+    var publishedGenDepthMax = _.max(publishedInstrumentJSONs, 'generation').generation;
+    var publishedGenDepthAvg = _.sum(_.map(publishedInstrumentJSONs, 'generation'), 'generation') / publishedInstrumentJSONs.length;
+    log('published generation depth: max: '+publishedGenDepthMax+', avg:'+publishedGenDepthAvg);
+
     var branchedGenerationMax = _.max(instruments, 'branchedGeneration').branchedGeneration;
-    var branchedGenerationAvg = _.sum(_.map(instruments, 'branchedGeneration'), 'branchedGeneration') / instruments.length;
+    var branchedGenerationAvg = _.sum(
+        _.map(instruments, 'branchedGeneration'),
+        'branchedGeneration') / instruments.length;
     log('generation depth: max: '+branchedGenerationMax+', avg:'+branchedGenerationAvg);
+
+    log();
+    log('evolution history all');
+    var evolutionHistory = _.reduce(_.reduce(instrumentJSONs,
+        function(result, instrument) {
+            Array.prototype.push.apply(result, instrument.evolutionHistory);
+            return result;
+        }, []), function(result, evolutionAction) {
+            if (result[evolutionAction]) {
+                ++result[evolutionAction];
+            } else {
+                result[evolutionAction] = 1;
+            }
+            return result;
+        }, {});
+    _.forEach(evolutionHistory, function(numUsed, action) {
+        log(action + ' '+numUsed);
+    });
+    log();
+    log('evolution history published');
+    var evolutionHistory = _.reduce(_.reduce(instruments,
+        function(result, instrument) {
+            if (!instrument.isPrivate) {
+                var json = JSON.parse(instrument.json);
+                Array.prototype.push.apply(result, json.evolutionHistory);
+            }
+            return result;
+        }, []), function(result, evolutionAction) {
+            if (result[evolutionAction]) {
+                ++result[evolutionAction];
+            } else {
+                result[evolutionAction] = 1;
+            }
+            return result;
+        }, {});
+    _.forEach(evolutionHistory, function(numUsed, action) {
+        log(action + ' '+numUsed);
+    });
 });
